a5496ca1850059a5e9873086d8246f2c3cae79db
agustin-d17 <agustindiaz449@gmail.com>
2024-09-09 16:58:54 -0300

    NEW COMPONENT - HomeSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar.jsx'
+import HomeSection from './components/HomeSection/HomeSection'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <HomeSection id="HomeSection"/>
     </>
   )
 }
