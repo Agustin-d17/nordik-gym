04a1183a09ee6703367bd7d002e48129c2bf8b36
agustin-d17 <agustindiaz449@gmail.com>
2024-09-13 16:20:40 -0300

    NEW COMPONENT - PricesSection

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar.jsx'
 import HomeSection from './components/HomeSection/HomeSection'
 import SheduleSection from './components/SheduleSection/SheduleSection'
+import PricesSection from './components/PricesSection/PricesSection'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <HomeSection />
       <SheduleSection />
+      <PricesSection />
     </>
   )
 }
