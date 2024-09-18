a236cc2dd2a49c788465742a408f0fcf58e4388f
agustin-d17 <agustindiaz449@gmail.com>
2024-09-18 17:56:55 -0300

    VISUAL CHANGES - Background HomeSection

--- src/components/HomeSection/HomeSection.jsx
+++ src/components/HomeSection/HomeSection.jsx
@@ -2,18 +2,21 @@ import { Button } from "@/components/ui/button"
 
 const HomeSection = () => {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center text-center px-4">
-      <h1 className="text-4xl font-bold mb-4 text-gray-800">
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center text-center px-4 bg-cover bg-[url('./src/assets/images/background.webp')]">
+      <h1 className="text-4xl font-bold mb-4 text-gray-100">
         Bienvenido a Nordik Gym
       </h1>
-      <p className="text-xl mb-8 text-gray-600 max-w-md">
+      <p className="text-xl mb-8 text-gray-100 max-w-md">
         Transforma tu cuerpo, mejora tu salud y alcanza tus metas con nosotros.
       </p>
       <Button className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-6 rounded-full text-lg">
         ¡Comienza Ahora!
       </Button>
     </div>
+    
+    
   )
 }
 
 export default HomeSection
+
